0689000e10dd312d88ae92527f3dabbcc9ce9c3b
JOTARO <380686356@qq.com>
2024-07-02 13:41:03 +0800

    feat:Disable button

--- src/components/ui/common/narbar/index.js
+++ src/components/ui/common/narbar/index.js
@@ -1,18 +1,25 @@
+'use client'
+
 import Link from "next/link"
 
 import { useWeb3 } from "@components/providers"
 
 import { Button } from "@components/ui/common"
 
+import { useRouter } from 'next/navigation'
+
 export default function Navbar() {
 
     const { connect, isLoading, isWeb3Loaded } = useWeb3()
+
+
+    const router = useRouter()
 
     return (
         <section>
             <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
                 <nav className="relative" aria-label="Global">
-                    <div className="flex justify-between">
+                    <div className="flex justify-between items-center">
                         <div>
                             <Link href="#" className="font-medium mr-8 text-gray-500 hover:text-gray-900">Home</Link>
                             <Link href="#" className="font-medium mr-8 text-gray-500 hover:text-gray-900">Marketplace</Link>
@@ -22,7 +29,9 @@ export default function Navbar() {
                             <Link href="#" className="font-medium mr-8 text-gray-500 hover:text-gray-900">Wishlist</Link>
                             {isLoading ?
                                 <Button
+                                    disabled={true}
                                     onClick={connect}>
+
                                     Loading...
                                 </Button> :
                                 isWeb3Loaded ?
@@ -31,7 +40,7 @@ export default function Navbar() {
                                         Connect
                                     </Button> :
                                     <Button
-                                        onClick={connect}>
+                                        onClick={() => router.push("https://metamask.io/download.html")}>
                                         Install Metamask
                                     </Button>
                             }
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,8 @@ module.exports = {
       maxWidth: {
         "8xl": "1920px"
       },
+      opacity: ["disabled"],
+      cursor: ["disabled"],
     },
   },
   plugins: [],
