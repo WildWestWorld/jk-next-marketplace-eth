51dc6677e2d1d59206f56cff3817c7b8e84111d5
JOTARO <380686356@qq.com>
2024-07-03 11:29:59 +0800

    feat:Display address

--- src/components/ui/common/footer/index.js
+++ src/components/ui/common/footer/index.js
@@ -1,36 +1,20 @@
 
 
 
-import Link from "next/link"
 
 export default function Footer() {
 
     return (
-        <section>
-            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-                <nav className="relative" aria-label="Global">
-                    <div className="flex justify-between">
-                        <div>
-                            <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                                Home
-                            </Link>
-                            <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-
-                                Marketplace
-                            </Link>
-                            <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                                Blogs
-                            </Link>
-                        </div>
-                        <div>
-                            <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                                Wishlist
-                            </Link>
-                            <a href="#" className="font-medium mr-8 text-indigo-600 hover:text-indigo-500">Log in</a>
-                        </div>
+        <footer className="bg-gray-900 pt-1">
+            <div className="container mx-auto px-6">
+                <div className="mt-5 flex flex-col items-center">
+                    <div className="py-6">
+                        <p className="mb-6 text-white text-sm text-primary-2 font-bold">
+                            © {new Date().getFullYear()} Eincode
+                        </p>
                     </div>
-                </nav>
-            </div >
-        </section >
+                </div>
+            </div>
+        </footer>
     )
 }
--- src/components/ui/common/narbar/index.js
+++ src/components/ui/common/narbar/index.js
@@ -13,7 +13,6 @@ export default function Navbar() {
 
     return (
         <section>
-            {account}
             <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
                 <nav className="relative" aria-label="Global">
                     <div className="flex justify-between items-center">
@@ -50,6 +49,15 @@ export default function Navbar() {
                     </div>
                 </nav>
             </div>
+
+
+            {account &&
+                <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
+                    <div className="text-white bg-indigo-600 rounded-md p-2">
+                        {account}
+                    </div>
+                </div>
+            }
         </section>
     )
 }
