e13b5a19da46bbd3013a04ab86797e1df14f2c55
JOTARO <380686356@qq.com>
2024-07-03 11:26:19 +0800

    feat:use account event

--- src/components/providers/web3/hooks/useAccount.js
+++ src/components/providers/web3/hooks/useAccount.js
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from "react"
 
-export const handler = web3 => () => {
+export const handler = (web3, provider) => () => {
     const [account, setAccount] = useState(null)
 
     useEffect(() => {
@@ -12,6 +12,14 @@ export const handler = web3 => () => {
 
         web3 && getAccount()
     }, [web3])
+
+
+    useEffect(() => {
+        provider &&
+            provider.on("accountsChanged",
+                accounts => setAccount(accounts[0] ?? null)
+            )
+    }, [provider])
 
     return { account }
 }
