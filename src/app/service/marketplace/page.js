3c2aa634fe1ad261e4157baf186c273b67c75329
JOTARO <380686356@qq.com>
2024-07-03 13:19:22 +0800

    feat:useNetwork

--- src/app/service/marketplace/page.js
+++ src/app/service/marketplace/page.js
@@ -6,12 +6,18 @@ import { getAllCourses } from "@content/courses/fetcher"
 import { WalletBar } from "@components/ui/web3"
 import { useAccount } from "@components/hooks/web3/useAccount"
 
+import { useNetwork } from "@components/hooks/web3/useNetwork"
+
 export default function Marketplace() {
     const { account } = useAccount()
+    const { network } = useNetwork()
+
+
     const { data } = getStaticProps()
     return (
         <>
             <div className="py-4">
+                {network.data}
                 <WalletBar
                     address={account.data}
                 />
