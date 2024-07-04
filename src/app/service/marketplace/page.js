bfd572b20b21af6dd5ba03f6d5fc5e93f86fbcd1
JOTARO <380686356@qq.com>
2024-07-04 12:31:01 +0800

    feat:Card button

--- src/app/service/marketplace/page.js
+++ src/app/service/marketplace/page.js
@@ -5,7 +5,7 @@ import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher"
 import { WalletBar } from "@components/ui/web3"
 import { useAccount, useNetwork } from "@components/hooks/web3"
-
+import { Button } from "@components/ui/common"
 
 export default function Marketplace() {
     const { account } = useAccount()
@@ -37,6 +37,13 @@ export default function Marketplace() {
                     <CourseCard
                         key={course.id}
                         course={course}
+                        Footer={() =>
+                            <div className="mt-4">
+                                <Button variant="lightPurple">
+                                    Purchase
+                                </Button>
+                            </div>
+                        }
                     />
                 }
             </CourseList>
