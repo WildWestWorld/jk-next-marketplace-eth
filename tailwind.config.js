bd50d76925068f7e7f3b57dc647a9f626cfd5c75
JOTARO <380686356@qq.com>
2024-07-05 16:46:39 +0800

    feat:Course card styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,9 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       flex: {
-        "2": "2 2 0%"
+        "2": "2 2 0%",
+        "3": "3 3 0%",
+        "4": "4 4 0%"
       },
       maxWidth: {
         "8xl": "1920px"
