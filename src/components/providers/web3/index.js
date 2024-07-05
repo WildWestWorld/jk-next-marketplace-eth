8c49d0df9be6e835f211ba2fe19236018ac25cb2
JOTARO <380686356@qq.com>
2024-07-05 13:12:22 +0800

    feat:Load contract

--- src/components/providers/web3/index.js
+++ src/components/providers/web3/index.js
@@ -23,7 +23,9 @@ export default function Web3Provider({ children }) {
             const provider = await detectEthereumProvider()
             if (provider) {
                 const web3 = new Web3(provider)
-                const contract = loadContract("CourseMarketplace", provider)
+
+                const contract = await loadContract("CourseMarketplace", web3)
+                console.log(contract)
                 setWeb3Api({
                     provider,
                     web3,
