1a6a9bbe25e66e41359e34f0b805d02cae96783b
JOTARO <380686356@qq.com>
2024-07-09 12:26:46 +0800

    feat:use admin 3

--- src/app/service/marketplace/courses/manage/managed.js
+++ src/app/service/marketplace/courses/manage/managed.js
@@ -1,6 +1,6 @@
 
 
-import { useAccount, useManagedCourses } from "@components/hooks/web3";
+import { useAdmin, useManagedCourses } from "@components/hooks/web3";
 import { useWeb3 } from "@components/providers";
 import { Button, Message } from "@components/ui/common";
 import { CourseFilter, ManagedCourseCard, OwnedCourseCard } from "@components/ui/course";
@@ -37,7 +37,7 @@ export default function ManagedCourses() {
     const [proofedOwnership, setProofedOwnership] = useState({})
     const { web3 } = useWeb3()
 
-    const { account } = useAccount()
+    const { account } = useAdmin({ redirectTo: "/marketplace" })
     const { managedCourses } = useManagedCourses(account)
 
     const verifyCourse = (email, { hash, proof }) => {
@@ -57,6 +57,10 @@ export default function ManagedCourses() {
                 [hash]: false
             })
 
+    }
+
+    if (!account.isAdmin) {
+        return null
     }
 
     return (
--- src/components/ui/marketplace/header/index.js
+++ src/components/ui/marketplace/header/index.js
@@ -1,9 +1,8 @@
 
 
+import { useAccount } from "@components/hooks/web3";
 import { Breadcrumbs } from "@components/ui/common";
 import { EthRates, WalletBar } from "@components/ui/web3";
-
-
 
 const LINKS = [{
     href: "/service/marketplace",
@@ -13,10 +12,12 @@ const LINKS = [{
     value: "My Courses"
 }, {
     href: "/service/marketplace/courses/managed",
-    value: "Manage Courses"
+    value: "Manage Courses",
+    requireAdmin: true
 }]
 
 export default function Header() {
+    const { account } = useAccount()
     return (
         <>
             <div className="pt-4">
@@ -24,7 +25,10 @@ export default function Header() {
             </div>
             <EthRates />
             <div className="flex flex-row-reverse p-4 sm:px-6 lg:px-8">
-                <Breadcrumbs items={LINKS} />
+                <Breadcrumbs
+                    isAdmin={account.isAdmin}
+                    items={LINKS}
+                />
             </div>
         </>
     )
