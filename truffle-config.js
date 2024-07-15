fb3b4a3ec0046e852938d866e17fb2a5cf02ff32
JOTARO <380686356@qq.com>
2024-07-15 15:02:13 +0800

    feat:Deployment to Ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const keys = require("./keys.json")
 
 module.exports = {
 
@@ -7,6 +9,15 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*",
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(keys.MNEMONIC, `https://sepolia.infura.io/v3/${keys.INFURA_PROJECT_ID}`, 2),
+      network_id: 11155111, // Sepolia's id
+      gas: 5500000, // Gas Limit, How much gas we are willing to spent
+      gasPrice: 20000000000, // how much we are willing to spent for unit of gas
+      confirmations: 2, // number of blocks to wait between deployment
+      timeoutBlocks: 200, // number of blocks before deployment times out
+      skipDryRun: true
     },
 
   },
